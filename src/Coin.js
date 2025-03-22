bd13c4f6539656042a517a73fc09e19eb155851e
manasvirana <manasvirana92@gmail.com>
2025-03-22 19:13:53 +0530

    fixedSSL

--- src/Coin.js
+++ src/Coin.js
@@ -4,18 +4,18 @@ import CoinChartData from './CoinChartData';
 import { IdContext } from './context/IdContext'
 
 const Coin = ({ id, image, name, symbol, price, volume, priceChange, marketcap }) => {
-
+    
     const [style, setStyle] = useState('chart-view-passive')
     // eslint-disable-next-line
     const [coinId, setCoinId] = useContext(IdContext)
-
+    
     const showGraph = () => {
         style === 'chart-view-passive' ?
             setStyle('chart-view-active') :
             setStyle('chart-view-passive')
         setCoinId(id)
     }
-
+    
     return (
         <div className='border-bottom'>
             <div className="coin-container">
@@ -26,17 +26,18 @@ const Coin = ({ id, image, name, symbol, price, volume, priceChange, marketcap }
                         <p className="coin-symbol">{symbol}</p>
                     </div>
                     <div className="coin-data">
-                        <p className="coin-price">₹ {price.toLocaleString('hi-IN')}</p>
-                        <p className="coin-volume">₹ {volume.toLocaleString('hi-IN')}</p>
-                        {priceChange < 0
-                            ? (
+                        <p className="coin-price">₹ {price ? price.toLocaleString('hi-IN') : '0'}</p>
+                        <p className="coin-volume">₹ {volume ? volume.toLocaleString('hi-IN') : '0'}</p>
+                        {priceChange ? (
+                            priceChange < 0 ? (
                                 <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
-                            )
-                            : (
+                            ) : (
                                 <p className="coin-percent green">+{priceChange.toFixed(2)}%</p>
                             )
-                        }
-                        <p className="coin-marketcap"> <span className="mktcap"> Market Cap :</span> ₹ {marketcap.toLocaleString('hi-IN')}</p>
+                        ) : (
+                            <p className="coin-percent">0.00%</p>
+                        )}
+                        <p className="coin-marketcap"> <span className="mktcap"> Market Cap :</span> ₹ {marketcap ? marketcap.toLocaleString('hi-IN') : '0'}</p>
                     </div>
                 </div>
             </div>
